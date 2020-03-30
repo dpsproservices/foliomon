834889ee99634541a659c649f5e99099c1614bad
krieg2 <aaronk1@yahoo.com>
2020-03-30 17:32:07 -0400

    Updaing UI routing

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -1,10 +1,12 @@
   
 import React, { forwardRef } from 'react';
 import clsx from 'clsx';
-//import { NavLink as RouterLink } from 'react-router-dom';
+import { NavLink as RouterLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
-import { Drawer, List, ListItem, Button } from '@material-ui/core';
+import { Drawer, List, ListItem, Button, colors } from '@material-ui/core';
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import ViewListIcon from '@material-ui/icons/ViewList';
+import InsertChartIcon from '@material-ui/icons/InsertChart';
 
 const useStyles = makeStyles(theme => ({
     drawer: {
@@ -26,17 +28,46 @@ const useStyles = makeStyles(theme => ({
     },
     nav: {
       marginBottom: theme.spacing(2)
+    },
+    item: {
+      display: 'flex',
+      paddingTop: 0,
+      paddingBottom: 0
+    },
+    button: {
+      color: colors.blueGrey[800],
+      padding: '10px 8px',
+      justifyContent: 'flex-start',
+      textTransform: 'none',
+      letterSpacing: 0,
+      width: '100%',
+      fontWeight: theme.typography.fontWeightMedium
+    },
+    icon: {
+      color: theme.palette.icon,
+      width: 24,
+      height: 24,
+      display: 'flex',
+      alignItems: 'center',
+      marginRight: theme.spacing(1)
+    },
+    active: {
+      color: theme.palette.primary.main,
+      fontWeight: theme.typography.fontWeightMedium,
+      '& $icon': {
+        color: theme.palette.primary.main
+      }
     }
 }));
 
-// const CustomRouterLink = forwardRef((props, ref) => (
-//   <div
-//     ref={ref}
-//     style={{ flexGrow: 1 }}
-//   >
-//     <RouterLink {...props} />
-//   </div>
-// ));
+const CustomRouterLink = forwardRef((props, ref) => (
+  <div
+    ref={ref}
+    style={{ flexGrow: 1 }}
+  >
+    <RouterLink {...props} />
+  </div>
+));
 
 const Sidebar = props => {
   const { open, variant, onClose, className } = props;
@@ -50,22 +81,52 @@ const Sidebar = props => {
       open={open}
       variant={variant}
     >
-      <List
-      className={clsx(classes.root, className)}
-      >
+      <List className={clsx(classes.root, classes.nav)}>
+        <ListItem
+          className={classes.item}
+          disableGutters
+          key="Overview"
+        >
+          <Button
+            activeClassName={classes.active}
+            className={classes.button}
+            component={CustomRouterLink}
+            to="/"
+          >
+            <div className={classes.icon}><DashboardIcon /></div>
+            Overview
+          </Button>
+        </ListItem>
+
         <ListItem
           className={classes.item}
           disableGutters
           key="Orders"
         >
           <Button
-            //activeClassName={classes.active}
+            activeClassName={classes.active}
             className={classes.button}
-            //component={CustomRouterLink}
-            href="/orders"
+            component={CustomRouterLink}
+            to="/orders"
           >
-            <div className={classes.icon}><DashboardIcon /></div>
+            <div className={classes.icon}><ViewListIcon /></div>
             Orders
+          </Button>
+        </ListItem>
+
+        <ListItem
+          className={classes.item}
+          disableGutters
+          key="Positions"
+        >
+          <Button
+            activeClassName={classes.active}
+            className={classes.button}
+            component={CustomRouterLink}
+            to="/"
+          >
+            <div className={classes.icon}><InsertChartIcon /></div>
+            Positions
           </Button>
         </ListItem>
       </List>
