09faf8c444b3570a9e0f62e7ddfabb5087fd6c89
krieg2 <aaronk1@yahoo.com>
2020-04-26 16:30:41 -0400

    Adding transactions to overview page

--- front-end/src/utils/utils.js
+++ front-end/src/utils/utils.js
@@ -3,3 +3,13 @@
 //export const PORT = process.env.PORT || 443;
 
 export const API_URL = `https://localhost:4000/foliomon`;
+
+export const convertCase = (str) => {
+  if (!str) return;
+
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const numberWithCommas = (x) => {
+  return x.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
