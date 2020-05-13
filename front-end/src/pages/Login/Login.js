b2b32c913c1b9c5215c5ca0fd71064690aaba292
Joseph Skarulis <josephskarulis@Skarulis-MBP.local>
2020-05-12 23:53:56 -0400

    Add Foliomon pie chart logo to login page.

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-
+import logo from './logo.png'; 
 import LoginForm from './LoginForm';
 import Copyright from '../../components/Copyright';
 
@@ -12,7 +12,7 @@ const Login = (props) => {
     return (
         <Grid container direction="column" alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
             <Box display="flex" alignItems="center" justifyContent="center">
-                <h1><strong>Foliomon</strong></h1>
+                <img src={logo} alt="logo" height="40px" width="40px" /><h1><strong>Foliomon</strong></h1>
             </Box>
             <Paper elevation={8}>
                 <Box width={400} height={400}>
